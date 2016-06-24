e1ee746d3e8933bda325e513c279bbd76e9eee6b
psjsaravana <psj.saravana@gmail.com>
2016-06-24 14:30:50 +0530

    app.js updated with mongodb

--- Server/app.js
+++ Server/app.js
@@ -10,6 +10,13 @@ var mongoose = require('mongoose');
 var app = express();
 var port = process.env.PORT || 8001;
 
+// configure mongodb with model
+mongoose.connect('mongodb://ssaravananmechengg@gmail.com:incorrect94@ds89.mLab.com:29017/notificationapp');
+var tokenSchema = new mongoose.Schema({
+    token: String
+});
+var tokenModel = mongoose.model('Tokens', tokenSchema);
+
 app.get('/sendnotification', function(req, res) {
     tokenModel.find({}).exec(function(err, result) {
         if (!err) {
@@ -27,6 +34,24 @@ app.get('/sendnotification', function(req, res) {
     });
 });
 
+//API for adding Device token into MongoDB
+app.put('/addtoken', function(req, res) {
+    var token = new tokenModel({ "token": req.body.token });
+    tokenModel.find({ "token": req.body.token }).exec(function(err, result) {
+        if (!err) {
+            if (result.length != 0) {
+                res.send({ "message": "Token already added" });
+            } else {
+                token.save(function(err) {
+                    if (err) res.send(err);
+                    res.send({ "message": "Token added successfully!" });
+                })
+            }
+        } else {
+            res.send({ "message": "Database error" });
+        };
+    });
+});
 
 app.use('/', function(req, res) {
   res.send('Server is Active!!!!!!!!!!');
